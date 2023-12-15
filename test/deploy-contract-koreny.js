8641a2d126e004ac7f5a6de1dc5f8bb915aaf598
Jose Manuel Cantera <jose.cantera@iota.org>
2023-12-15 09:25:52 +0100

    Allow to set the ERC20 token address

--- test/deploy-contract-koreny.js
+++ test/deploy-contract-koreny.js
@@ -8,28 +8,10 @@ const Web3 = require("web3");
 const devContractArtifact = require("../smart-contract-templates/build/contracts/Contract.json");
 const devContractArtifact2 = require("../smart-contract-templates/build/contracts/Contract2.json");
 
-
 // Create a `.env` file containing these informations:
 //IOTA_WASP_MNEMONIC="angle ...... meadow cereal"
 //IOTA_EVM_ENDPOINT_URL="http://localhost"
 require("dotenv").config();
-
-const phrase = process.env.IOTA_WASP_MNEMONIC;
-
-const providerUrl = process.env.IOTA_EVM_ENDPOINT_URL;
-
-const chainId = process.env.IOTA_WASP_CHAIN_ID;
-const ipfsGatewayUrl = process.env.IPFS_GATEWAY_URL;
-
-const { MCO_JSON_FILE, PRIVATE_KEYS_FILE, TEST_ADDR, CONTRACT_TEMPLATE } = process.env;
-
-let contractTemplate = devContractArtifact;
-
-if (CONTRACT_TEMPLATE) {
-    if (CONTRACT_TEMPLATE === "cascade") {
-        contractTemplate = devContractArtifact2;
-    }
-}
 
 function usage() {
     console.error(
@@ -39,50 +21,82 @@ function usage() {
     process.exit(-1);
 }
 
-if (!MCO_JSON_FILE) {
-    usage();
+let provider;
+let mco;
+let contractTemplate = devContractArtifact;
+let walletService;
+let ipfsGatewayUrl;
+let chainId;
+
+const {
+    IOTA_WASP_MNEMONIC,
+    IOTA_EVM_ENDPOINT_URL,
+    IOTA_WASP_CHAIN_ID,
+    IPFS_GATEWAY_URL,
+    NFT_SMART_CONTRACT_ADDR,
+    ERC20_TOKEN_SMART_CONTRACT_ADDR,
+} = process.env;
+
+const { MCO_JSON_FILE, PRIVATE_KEYS_FILE, TEST_ADDR, CONTRACT_TEMPLATE } = process.env;
+
+function prepare() {
+    const phrase = IOTA_WASP_MNEMONIC;
+    const providerUrl = IOTA_EVM_ENDPOINT_URL;
+    chainId = IOTA_WASP_CHAIN_ID;
+    ipfsGatewayUrl = IPFS_GATEWAY_URL;
+
+    if (!MCO_JSON_FILE) {
+        usage();
+    }
+    mco = JSON.parse(fs.readFileSync(MCO_JSON_FILE));
+
+    if (CONTRACT_TEMPLATE) {
+        if (CONTRACT_TEMPLATE !== "cascade" && CONTRACT_TEMPLATE !== "default") {
+            usage();
+        }
+
+        if (CONTRACT_TEMPLATE === "cascade") {
+            contractTemplate = devContractArtifact2;
+        }
+    }
+
+    walletService = new WalletService(process.env.WALLET_SERVICE_URL, process.env.WALLET_SERVICE_TOKEN);
+
+    let privateKeys = [];
+
+    if (PRIVATE_KEYS_FILE) {
+        try {
+            const content = fs.readFileSync(PRIVATE_KEYS_FILE);
+            privateKeys = JSON.parse(content);
+        } catch {
+            console.warn("No private keys supplied. Using mnemonic phrase");
+        }
+    }
+
+    if (privateKeys.length === 0) {
+        if (!phrase) {
+            console.error("No private keys not mnemonic has been supplied!!!");
+            process.exit(-1);
+        }
+        provider = new HDWalletProvider({
+            mnemonic: {
+                phrase,
+            },
+            providerOrUrl: providerUrl,
+        });
+    } else {
+        provider = new HDWalletProvider({
+            privateKeys,
+            providerOrUrl: providerUrl,
+        });
+
+        console.log("Using private key file. Addresses: ", provider.getAddresses());
+    }
 }
 
-const mco = JSON.parse(fs.readFileSync(MCO_JSON_FILE));
-
-const generate = () => {
+const generate = (mco) => {
     return generateSmartContractSpecification(mco.mediaContractualObjects.contracts[0]);
 };
-
-const walletService = new WalletService(process.env.WALLET_SERVICE_URL, process.env.WALLET_SERVICE_TOKEN);
-
-let privateKeys = [];
-
-if (PRIVATE_KEYS_FILE) {
-    try {
-        const content = fs.readFileSync(PRIVATE_KEYS_FILE);
-        privateKeys = JSON.parse(content);
-    } catch {
-        console.warn("No private keys supplied. Using mnemonic phrase");
-    }
-}
-
-let provider;
-
-if (privateKeys.length === 0) {
-    if (!phrase) {
-        console.error("No private keys not mnemonic has been supplied!!!");
-        process.exit(-1);
-    }
-    provider = new HDWalletProvider({
-        mnemonic: {
-            phrase,
-        },
-        providerOrUrl: providerUrl,
-    });
-} else {
-    provider = new HDWalletProvider({
-        privateKeys,
-        providerOrUrl: providerUrl,
-    });
-
-    console.log("Using private key file. Addresses: ", provider.getAddresses());
-}
 
 const deploy = async (smartContractSpecification, ipfs) => {
     const deployer = new EthereumDeployer(provider, ipfs, chainId, contractTemplate);
@@ -94,6 +108,13 @@ const deploy = async (smartContractSpecification, ipfs) => {
     await deployer.setMainAddressIndex(0);
 
     await deployer.fetchEvmAccountForParties();
+
+    if (NFT_SMART_CONTRACT_ADDR) {
+        deployer.setNFTokenContractAddress(NFT_SMART_CONTRACT_ADDR);
+    }
+    if (ERC20_TOKEN_SMART_CONTRACT_ADDR) {
+        deployer.setERC20TokenContractAddress(ERC20_TOKEN_SMART_CONTRACT_ADDR);
+    }
 
     const res = await deployer.deploySmartContracts();
 
@@ -124,7 +145,9 @@ const parse = async (address, ipfs) => {
 };
 
 const main = async () => {
-    const smartContractSpecification = generate();
+    prepare();
+
+    const smartContractSpecification = generate(mco);
     const ipfs = new OffChainStorage(ipfsGatewayUrl);
     await ipfs.start();
     let address = await deploy(smartContractSpecification, ipfs);
